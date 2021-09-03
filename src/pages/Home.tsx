c6183c905c15d73494f3a503ef3595f9203418e3
EtheliusDevelopment <etheliusdev@gmail.com>
2021-09-03 15:21:18 +0100

    PAGE LAYOUTING  DESKTOP INIT

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,54 +4,53 @@ import { Wrapper } from "./Home.styles";
 const Home = () => {
   return (
     <Wrapper>
-      <div className="first-block">
-        <h1 className="h1-first-block">
+      <div className="first-block div-block">
+        <h1 className="h1-title-block">
           Lorem ipsum dolor sit amet consectetur.
         </h1>
-        <p className="p-first-block">
+        <p className="p-par-block">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
           harum, expedita magnam amet reprehenderit cum id modi sunt tempore
           nisi distinctio adipisci laboriosam iste exercitationem fugiat alias
           doloremque cupiditate quidem?
         </p>
       </div>
-    
-      <div className="second-block">
-        <h1 className="h1-second-block">
+
+      <div className="second-block div-block">
+        <h1 className="h1-title-block">
           Lorem ipsum dolor sit amet consectetur.
         </h1>
-        <p className="h1-second-block">
+        <p className="p-par-block">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
           harum, expedita magnam amet reprehenderit cum id modi sunt tempore
           nisi distinctio adipisci laboriosam iste exercitationem fugiat alias
           doloremque cupiditate quidem?
         </p>
       </div>
-    
-      <div className="third-block">
-        <h1 className="h1-third-block">
+
+      <div className="third-block div-block">
+        <h1 className="h1-title-block">
           Lorem ipsum dolor sit amet consectetur.
         </h1>
-        <p className="h1-third-block">
+        <p className="p-par-block">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
           harum, expedita magnam amet reprehenderit cum id modi sunt tempore
           nisi distinctio adipisci laboriosam iste exercitationem fugiat alias
           doloremque cupiditate quidem?
         </p>
       </div>
-    
-      <div className="fourth-block">
-        <h1 className="h1-fourth-block">
+
+      <div className="fourth-block div-block">
+        <h1 className="h1-title-block">
           Lorem ipsum dolor sit amet consectetur.
         </h1>
-        <p className="h1-fourth-block">
+        <p className="p-par-block">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
           harum, expedita magnam amet reprehenderit cum id modi sunt tempore
           nisi distinctio adipisci laboriosam iste exercitationem fugiat alias
           doloremque cupiditate quidem?
         </p>
       </div>
-    
     </Wrapper>
   );
 };
--- src/pages/Moments.tsx
+++ src/pages/Moments.tsx
@@ -1,57 +1,55 @@
-
 const Moments = () => {
-    return (
-        <>
-        <div className="first-block">
-          <h1 className="h1-first-block">
-            Lorem ipsum dolor sit amet consectetur.
-          </h1>
-          <p className="h1-first-block">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
-            harum, expedita magnam amet reprehenderit cum id modi sunt tempore
-            nisi distinctio adipisci laboriosam iste exercitationem fugiat alias
-            doloremque cupiditate quidem?
-          </p>
-        </div>
-      
-        <div className="second-block">
-          <h1 className="h1-second-block">
-            Lorem ipsum dolor sit amet consectetur.
-          </h1>
-          <p className="h1-second-block">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
-            harum, expedita magnam amet reprehenderit cum id modi sunt tempore
-            nisi distinctio adipisci laboriosam iste exercitationem fugiat alias
-            doloremque cupiditate quidem?
-          </p>
-        </div>
-      
-        <div className="third-block">
-          <h1 className="h1-third-block">
-            Lorem ipsum dolor sit amet consectetur.
-          </h1>
-          <p className="h1-third-block">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
-            harum, expedita magnam amet reprehenderit cum id modi sunt tempore
-            nisi distinctio adipisci laboriosam iste exercitationem fugiat alias
-            doloremque cupiditate quidem?
-          </p>
-        </div>
-      
-        <div className="fourth-block">
-          <h1 className="h1-fourth-block">
-            Lorem ipsum dolor sit amet consectetur.
-          </h1>
-          <p className="h1-fourth-block">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
-            harum, expedita magnam amet reprehenderit cum id modi sunt tempore
-            nisi distinctio adipisci laboriosam iste exercitationem fugiat alias
-            doloremque cupiditate quidem?
-          </p>
-        </div>
-      
-      </>
-    )
-}
+  return (
+    <>
+      <div className="first-block div-block">
+        <h1 className="h1-title-block">
+          Lorem ipsum dolor sit amet consectetur.
+        </h1>
+        <p className="p-par-block">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
+          harum, expedita magnam amet reprehenderit cum id modi sunt tempore
+          nisi distinctio adipisci laboriosam iste exercitationem fugiat alias
+          doloremque cupiditate quidem?
+        </p>
+      </div>
 
-export default Moments
+      <div className="second-block div-block">
+        <h1 className="h1-title-block">
+          Lorem ipsum dolor sit amet consectetur.
+        </h1>
+        <p className="p-par-block">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
+          harum, expedita magnam amet reprehenderit cum id modi sunt tempore
+          nisi distinctio adipisci laboriosam iste exercitationem fugiat alias
+          doloremque cupiditate quidem?
+        </p>
+      </div>
+
+      <div className="third-block div-block">
+        <h1 className="h1-title-block">
+          Lorem ipsum dolor sit amet consectetur.
+        </h1>
+        <p className="p-par-block">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
+          harum, expedita magnam amet reprehenderit cum id modi sunt tempore
+          nisi distinctio adipisci laboriosam iste exercitationem fugiat alias
+          doloremque cupiditate quidem?
+        </p>
+      </div>
+
+      <div className="fourth-block div-block">
+        <h1 className="h1-title-block">
+          Lorem ipsum dolor sit amet consectetur.
+        </h1>
+        <p className="p-par-block">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
+          harum, expedita magnam amet reprehenderit cum id modi sunt tempore
+          nisi distinctio adipisci laboriosam iste exercitationem fugiat alias
+          doloremque cupiditate quidem?
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Moments;
