36f17ec8795e9c0766da5966bf68034711804f55
EtheliusDevelopment <etheliusdev@gmail.com>
2021-09-03 11:18:25 +0100

    INIT COMMIT

--- src/components/NavBar/Navigation.tsx
+++ src/components/NavBar/Navigation.tsx
@@ -40,7 +40,7 @@ export default function SimpleBottomNavigation() {
         />
 
         <BottomNavigationAction
-          label="@AboutMe"
+          label="@About Me"
           icon={<FavoriteIcon />}
           component={Link}
           to="/about-me"
